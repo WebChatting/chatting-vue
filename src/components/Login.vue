5639d1a622add6c8beb16a05003fa73be41bd785
Rekord <1324596506@qq.com>
2023-02-10 14:14:01 +0800

    refactor: implement login logic with vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <link rel="stylesheet" href="css/account.css?v=65">
+        <div v-show="isloading" class="loading" id="loading"> +
+            <img src="img/loading.gif"> +
+        </div>
         <div class="account-container">
             <div class="left-text-container">
                 <h2>chatting</h2>
@@ -20,17 +23,20 @@
                         <div class="list">
                         <input type="text" class="input-text the-signin-account" placeholder="账号" data-errortext="请输入格式正确的账号"
                             data-type="account" oninput="javascript:void(0)" onpropertychange="javascript:void(0)" data-index="0"
-                            autocomplete="on">
+                            autocomplete="on"
+                            ref="accountInput">
                         </div>
                         <div class="list">
                         <input type="password" class="input-text the-signin-password" placeholder="密码" data-errortext="密码长度为6-20"
                             data-type="password" oninput="javascript:void(0)" onpropertychange="javascript:void(0)" data-index="1"
-                            id="signin-password" autocomplete="on">
+                            id="signin-password" autocomplete="on"
+                            ref="passwordInput">
                         <a href="#" class="right-text clickable" id="forget-password">忘记密码</a>
                         </div>
                         <div class="submit">
-                        <div class="error-text"></div>
-                        <div class="button" id="normal-login">登录</div>
+                        <div class="error-text">{{errorText}}</div>
+                        <div class="button" id="normal-login"
+                          @click="login" :class="classObj">{{loginText}}</div>
                         </div>
                     </div>
                     <!----------------- over ----------------->
@@ -50,7 +56,64 @@
 </template>
 
 <script>
-    
+    import axios from 'axios';
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                isloading: false,
+                classObj:{
+                  disabled:false
+                },
+                loginText: '登录',
+                errorText: ''
+            }
+        },
+        methods: {
+            showLoading() {
+                setTimeout(() => {
+                    this.isloading = true
+                }, 200);
+            },
+            hideLoading() {
+                setTimeout(() => {
+                    this.isloading = false;
+                }, 500);
+            },
+            login() {
+              if (this.classObj.disabled) {
+                return
+              }
+              this.showLoading()
+              this.classObj.disabled = true
+              this.loginText = "登录中..."
+
+              // 发送后端请求
+              axios({
+                method: 'post',
+                url: '/chatting/login',
+                params: {
+                  username: this.$refs.accountInput.value.trim(),
+                  password: this.$refs.passwordInput.value.trim()
+                },
+                responseType: 'json',
+              }).then((response) => {
+                this.loginText = "登录"
+                this.hideLoading()
+                this.classObj.disabled = false
+                if (response.data.status == 200) {
+                  window.location.href = "chatroom";
+                } else {
+                  this.errorText = response.data.msg;
+                }
+              })
+
+            }
+        },
+        mounted() {
+          this.$refs.accountInput.focus()
+        }
+    }
 </script>
 
 <style scoped>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: "http://localhost:8088"
+  }
 })

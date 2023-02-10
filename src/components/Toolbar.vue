fdec4ad0aa904b8dbfd0a977c9e615846827a481
Rekord <1324596506@qq.com>
2023-02-10 21:28:57 +0800

    style: fine-tune style for toolbar button

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -7,21 +7,21 @@
         <div id="btnBar">
             <div class="topBtnBar">
                 <el-tooltip  class="item" effect="dark" content="群聊" placement="right">
-                    <el-button class="toolBtn" size="small"><i class="el-icon-s-comment"></i></el-button>
+                    <el-button class="toolBtn" size="large"><i class="el-icon-s-comment"></i></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="用户列表" placement="right">
-                    <el-button class="toolBtn" size="small"><i class="el-icon-user-solid"></i></el-button>
+                    <el-button class="toolBtn" size="large"><i class="el-icon-user-solid"></i></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="与机器人聊天" placement="right">
-                    <el-button class="toolBtn" size="small"><i class="el-icon-s-platform"></i></el-button>
+                    <el-button class="toolBtn" size="large"><i class="el-icon-s-platform"></i></el-button>
                 </el-tooltip>
             </div>
             <div class="bottomBtnBar">
                 <el-tooltip class="item" effect="dark" content="设置" placement="right">
-                    <el-button class="toolBtn" size="small"><i class="el-icon-s-tools"></i></el-button>
+                    <el-button class="toolBtn" size="large"><i class="el-icon-s-tools"></i></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="退出" placement="right">
-                    <el-button class="toolBtn" size="small"><i class="el-icon-s-fold"></i></el-button>
+                    <el-button class="toolBtn" size="large"><i class="el-icon-s-fold"></i></el-button>
                 </el-tooltip>
             </div>
         </div>
@@ -51,7 +51,7 @@
     .toolBtn {
       background-color: #2e3238;
       border: 0;
-      margin: 5px 5px;
+      margin: 5px;
     }
   }
 </style>

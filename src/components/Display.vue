77216139b1ca56106780e1541a363e7d93152d0a
Rekord <1324596506@qq.com>
2023-02-11 14:58:05 +0800

    feat: finish private chat layout

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,13 +1,125 @@
 <template>
-    <div></div>
+    <div id="display">
+        <ul >
+            <li v-for="entry in messages" :key="entry.id">
+                <p class="time">
+                    <span>{{new Date()}}</span>
+                </p>
+                <div class="main" :class="{left: entry.style.left, right: entry.style.right}">
+                    <el-avatar class="avatar" :src="entry.userProfile"
+                        @error="false" shape="circle" :size="30" alt="Avatar">
+                        <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
+                    </el-avatar>
+                    <p v-if="entry.messageTypeId == 1" class="text">{{entry.content}}</p>
+                    <img v-if="entry.messageTypeId == 2" :src="entry.content" class="img">
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
         name: "Display",
+        data() {
+            return {
+                messages: [
+                    {
+                        id: 1,
+                        style: {
+                            left: 'left'
+                        },
+                        fromNickname: 'rekord',
+                        userProfile: 'https://cdn.sxrekord.com/blog/logo.jpg',
+                        messageTypeId: 1,
+                        content: 'hei',
+                    },
+                    {
+                        id: 2,
+                        style: {
+                            right: 'right'
+                        },
+                        fromNickname: 'rekord',
+                        userProfile: 'https://cdn.sxrekord.com/blog/logo.jpg',
+                        messageTypeId: 1,
+                        content: '你好',
+                    },
+                    {
+                        id: 3,
+                        style: {
+                            left: 'left'
+                        },
+                        fromNickname: 'rekord',
+                        userProfile: 'https://cdn.sxrekord.com/blog/logo.jpg',
+                        messageTypeId: 1,
+                        content: '长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长',
+                    }
+                ]
+            }
+        },
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    #display {
+        padding: 15px;
+        padding-right: 3px;
+        height: 80%;
+        max-height: 80%;
+        overflow-y: scroll;
+        overflow-x: hidden;
+        ul {
+            list-style-type: none;
+		    padding-left: 0px;
+            li {
+                margin-bottom: 10px;
+            }
+        }
+        .time {
+            text-align: center;
+            margin: 3px 0px;
+            > span {
+                display: inline-block;
+                padding: 0 18px;
+                font-size: 12px;
+                color: #FFF;
+                background-color: #dcdcdc;
+                border-radius: 3px;
+            }
+        }
+        .main {
+            display: -webkit-flex; /* Safari */
+            display: flex;
+            .avatar {
+                margin: 0;
+            }
+            .text {
+                margin: 0px;
+                display: inline-block;
+                padding: 0 10px;
+                max-width: 80%;
+                background-color: #fafafa;
+                border-radius: 8px;
+                line-height: 30px;
+            }
+            .img {
+                display: inline-block;
+                height: 100px;
+                width: 100px;
+                margin-top: 15px;
+            }
+        }
+        .left {
+            flex-direction: row;
+            .avatar {
+                margin-right: 10px;
+            }
+        }
+        .right {
+            flex-direction: row-reverse;
+            .avatar {
+                margin-left: 10px;
+            }
+        }
+    }
 </style>

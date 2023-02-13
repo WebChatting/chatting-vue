f245fe84fe366c2c949308d8dd931ef3d7c4af4e
Rekord <1324596506@qq.com>
2023-02-14 00:24:45 +0800

    refactor: refactor toolbar layout

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,34 +1,44 @@
 <template>
     <div id="toolbar">
-        <el-avatar class="imgProfile" src="https://cdn.sxrekord.com/blog/logo.jpg"
-            @error="false" :size="45" alt="Avatar">
-            <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
-        </el-avatar>
-        <div id="btnBar">
-            <div class="topBtnBar">
-                <el-tooltip class="item" effect="dark" content="联系人" placement="left">
-                    <el-button class="toolBtn" size="medium"
+        <div class="avatar">
+            <el-avatar src="https://cdn.sxrekord.com/blog/logo.jpg"
+                @error="false" :size="45" alt="Avatar">
+                <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
+            </el-avatar>
+        </div>
+        <div class="bar">
+            <div class="top-bar">
+                <el-tooltip effect="dark" content="联系人" placement="left">
+                    <el-button class="tool-button"
                         @click="loadContact">
                         <i class="el-icon-user-solid"></i>
                     </el-button>
                 </el-tooltip>
-                <el-tooltip class="item" effect="dark" content="验证" placement="left">
-                    <el-button class="toolBtn" size="medium"
+                <el-tooltip effect="dark" content="验证" placement="left">
+                    <el-button class="tool-button"
                         @click="loadVerification">
                         <i class="el-icon-message-solid"></i>
                     </el-button>
                 </el-tooltip>
-                <el-tooltip class="item" effect="dark" content="ChatGPT" placement="left">
-                    <el-button class="toolBtn" size="medium"><i class="el-icon-s-platform"></i></el-button>
+                <el-tooltip effect="dark" content="ChatGPT" placement="left">
+                    <el-button class="tool-button"><i class="el-icon-s-platform"></i></el-button>
                 </el-tooltip>
             </div>
-            <div class="bottomBtnBar">
-                <el-tooltip class="item" effect="dark" content="设置" placement="right">
-                    <el-button class="toolBtn" size="medium"><i class="el-icon-s-tools"></i></el-button>
-                </el-tooltip>
-                <el-tooltip class="item" effect="dark" content="退出" placement="right">
-                    <el-button class="toolBtn" size="medium"
-                        @click="exitSystem"><i class="el-icon-s-fold"></i></el-button>
+            <div class="bottom-bar">
+                <el-popover
+                    placement="left"
+                    width="100"
+                    trigger="hover">
+                    <el-switch
+                        v-model="mode"
+                        active-color="#13ce66"
+                        inactive-color="#595b5d"
+                        :active-text="currentMode">
+                    </el-switch>
+                    <el-button class="tool-button" slot="reference"><i class="el-icon-s-tools"></i></el-button>
+                </el-popover>
+                <el-tooltip effect="dark" content="退出" placement="right">
+                    <el-button class="tool-button" @click="exitSystem"><i class="el-icon-s-fold"></i></el-button>
                 </el-tooltip>
             </div>
         </div>
@@ -38,6 +48,16 @@
 <script>
     export default {
         name: 'Toolbar',
+        data() {
+            return {
+                mode: false,
+            }
+        },
+        computed: {
+            currentMode() {
+                return this.mode ? 'dark' : 'light'
+            },
+        },
         methods: {
             loadContact() {
                 this.$bus.$emit('loadContact')
@@ -45,6 +65,7 @@
             loadVerification() {
                 this.$bus.$emit('loadVerification')
             },
+            // 退出系统
             exitSystem() {
                 window.location.href = "/"
             },
@@ -54,22 +75,26 @@
 
 <style lang="scss" scoped>
   #toolbar {
-    width: 100%;
     height: 100%;
-    #btnBar {
-      width: 100%;
-      height: 85%;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
+    background-color: #2e3238;
+    .avatar {
+        text-align: center;
+        height: 45px;
+        margin-top: 5px;
     }
-    .imgProfile {
-      margin: 25px 8px;
-    }
-    .toolBtn {
-      background-color: #2e3238;
-      border: 0;
-      margin: 5px;
+    .bar {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 550px;
+        margin-top: 50px;
+        .tool-button {
+            background-color: #2e3238;
+            border: none;
+            width: 100%;
+            margin: 0;
+            margin: 5px 0;
+        }
     }
   }
 </style>

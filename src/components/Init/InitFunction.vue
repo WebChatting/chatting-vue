5a7264a7e25dd1f9b403c292499099ad22dbd30e
Rekord <1324596506@qq.com>
2023-03-29 19:51:52 +0800

    build: pass npm run lint

--- src/components/Init/InitFunction.vue
+++ src/components/Init/InitFunction.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <div></div>
 </template>
 
 <script>
@@ -7,18 +7,18 @@ import { wsType2messageType } from "@/utils/common";
 export default {
     name: "InitFunction",
     mounted() {
-        this.$store.state.socket.receive = message => {
+        this.$store.state.socket.receive = (message) => {
             let m = JSON.parse(message.data);
             if (m.ws_type > 2 && m.ws_type < 9) {
                 this.$notify({
-                    title: 'new message',
+                    title: "new message",
                     message: m.content,
-                })
+                });
                 m["contentType"] = wsType2messageType(m.ws_type);
-                this.$store.state.messages["user" + m.fromId].push(m)
+                this.$store.state.messages["user" + m.fromId].push(m);
                 this.$bus.$emit("scrollToBottom");
             }
-        }
+        };
     },
-}
+};
 </script>

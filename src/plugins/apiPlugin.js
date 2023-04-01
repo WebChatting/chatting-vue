6e05058463791fc43ad71ad33cf4289d2b84fcae
Rekord <1324596506@qq.com>
2023-04-01 22:12:04 +0800

    refactor: take login and register request to plugin

--- src/plugins/apiPlugin.js
+++ src/plugins/apiPlugin.js
@@ -1,9 +1,22 @@
 // apiPlugin.js
+import { post } from "@/service/request";
+import { USER_LOGIN_URL, USER_REGISTER_URL } from "@/config/api";
 
 export default {
     install(Vue) {
         Vue.prototype.$api = {
-            loginUser() {},
+            loginUser(username, password) {
+                return post(USER_LOGIN_URL, {
+                    username,
+                    password,
+                })
+            },
+            registerUser(username, password) {
+                return post(USER_REGISTER_URL, {
+                    username,
+                    password,
+                });
+            },
         };
     },
 };

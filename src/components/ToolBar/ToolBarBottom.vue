f66fb9b3ccdbf5697c7d2a898cfb4394a55d14e7
Rekord <1324596506@qq.com>
2023-04-23 21:23:05 +0800

    refactor: separate EditDrawer component

--- src/components/ToolBar/ToolBarBottom.vue
+++ src/components/ToolBar/ToolBarBottom.vue
@@ -13,14 +13,14 @@
                     <el-button
                         icon="el-icon-edit"
                         class="pop-button"
-                        @click="openEditInfo"
+                        @click="openDrawer(0)"
                     ></el-button>
                 </div>
                 <div>
                     <el-button
                         icon="el-icon-s-opportunity"
                         class="pop-button"
-                        @click="openCreateGroup"
+                        @click="openDrawer(1)"
                     ></el-button>
                 </div>
                 <div>
@@ -32,10 +32,15 @@
                 <div>
                     <el-dropdown @command="handleCommand" trigger="click">
                         <span class="el-dropdown-link">
-                            {{ locales[currentLocale].name }}<i class="el-icon-arrow-down el-icon--right"></i>
+                            {{ locales[currentLocale].name
+                            }}<i class="el-icon-arrow-down el-icon--right"></i>
                         </span>
                         <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item v-for="(locale, index) in locales" :key="index" :command="locale.code">
+                            <el-dropdown-item
+                                v-for="(locale, index) in locales"
+                                :key="index"
+                                :command="locale.code"
+                            >
                                 {{ locale.name }}
                             </el-dropdown-item>
                         </el-dropdown-menu>
@@ -52,151 +57,30 @@
             ></el-button>
         </el-tooltip>
 
-        <!-- 个人信息抽屉结构 -->
-        <el-drawer
-            title="个人信息"
-            :before-close="beforeCloseEditInfo"
-            :visible.sync="userDrawerSwitch"
-            direction="ltr"
-            ref="drawer"
-        >
-            <div class="drawer-content">
-                <div class="drawer-body">
-                    <el-form :model="userInfo">
-                        <!-- 头像 -->
-                        <el-form-item label="头像：" label-width="60px">
-                            <el-upload
-                                class="avatar-uploader"
-                                :action="file_upload_url"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload"
-                            >
-                                <img
-                                    v-if="userInfo.avatarPath"
-                                    :src="userInfo.avatarPath"
-                                    class="drawer-avatar"
-                                />
-                                <i
-                                    v-else
-                                    class="el-icon-plus avatar-uploader-icon"
-                                ></i>
-                            </el-upload>
-                        </el-form-item>
+        <!-- 编辑用户信息抽屉 -->
+        <edit-drawer :isUser="true" :drawerSwitch="userDrawerSwitch" />
 
-                        <el-form-item label="账号：" label-width="60px">
-                            <el-input
-                                v-model="userInfo.name"
-                                autocomplete="off"
-                            ></el-input>
-                        </el-form-item>
-                        <el-form-item
-                            label="密码："
-                            prop="pass"
-                            label-width="60px"
-                        >
-                            <el-input
-                                type="password"
-                                v-model="userInfo.password"
-                                autocomplete="off"
-                            ></el-input>
-                        </el-form-item>
-                    </el-form>
-                </div>
-                <div class="drawer-footer">
-                    <el-button @click="cancelForm" class="drawer-button"
-                        >取 消</el-button
-                    >
-                    <el-button
-                        type="primary"
-                        @click="$refs.drawer.closeDrawer()"
-                        :loading="loading"
-                        class="drawer-button"
-                        >{{ loading ? "提交中 ..." : "确 定" }}</el-button
-                    >
-                </div>
-            </div>
-        </el-drawer>
-
-        <!-- 创建群组抽屉结构 -->
-        <el-drawer
-            title="创建群组"
-            :before-close="beforeCloseCreateGroup"
-            :visible.sync="groupDrawerSwitch"
-            direction="ltr"
-            ref="groupDrawer"
-        >
-            <div class="drawer-content">
-                <div class="drawer-body">
-                    <el-form :model="groupInfo">
-                        <!-- 头像 -->
-                        <el-form-item label="头像：" label-width="60px">
-                            <el-upload
-                                class="avatar-uploader"
-                                :action="file_upload_url"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload"
-                            >
-                                <img
-                                    v-if="groupInfo.avatarPath"
-                                    :src="groupInfo.avatarPath"
-                                    class="drawer-avatar"
-                                />
-                                <i
-                                    v-else
-                                    class="el-icon-plus avatar-uploader-icon"
-                                ></i>
-                            </el-upload>
-                        </el-form-item>
-
-                        <el-form-item label="名称：" label-width="60px">
-                            <el-input
-                                v-model="groupInfo.name"
-                                autocomplete="off"
-                            ></el-input>
-                        </el-form-item>
-                    </el-form>
-                </div>
-                <div class="drawer-footer">
-                    <el-button @click="cancelForm" class="drawer-button"
-                        >取 消</el-button
-                    >
-                    <el-button
-                        type="primary"
-                        @click="$refs.groupDrawer.closeDrawer()"
-                        :loading="loading"
-                        class="drawer-button"
-                        >{{ loading ? "创建中 ..." : "创 建" }}</el-button
-                    >
-                </div>
-            </div>
-        </el-drawer>
+        <!-- 创建群组抽屉 -->
+        <edit-drawer :isUser="false" :drawerSwitch="groupDrawerSwitch" />
     </div>
 </template>
 
 <script>
-import { API_BASE_URL, FILE_UPLOAD_API_URL } from "@/config/api";
+import EditDrawer from "@/components/common/EditDrawer";
 import locales from "@/locales";
 import i18nMixin from "@/mixins/i18nMixin";
 export default {
     name: "ToolBarBottom",
     mixins: [i18nMixin],
+    components: {
+        EditDrawer,
+    },
     data() {
         return {
-            file_upload_url: API_BASE_URL + FILE_UPLOAD_API_URL,
-            userInfo: {},
-            groupInfo: {
-                avatarPath: "",
-                name: "",
-            },
             mode: false,
-            userDrawerSwitch: false,
-            groupDrawerSwitch: false,
-            loading: false,
-            timer: null,
-            user: JSON.parse(window.sessionStorage.getItem("user")),
             locales,
+            userDrawerSwitch: { val: false },
+            groupDrawerSwitch: { val: false },
         };
     },
     computed: {
@@ -212,109 +96,14 @@ export default {
             window.sessionStorage.removeItem("user");
             window.location.href = "/";
         },
-        openEditInfo() {
-            // 自动填充数据
-            this.userInfo = {
-                avatarPath: this.user.avatarPath,
-                name: this.user.username,
-                password: this.user.password,
-            };
-
+        openDrawer(type) {
             // 关闭右侧面板
             this.$bus.$emit("closeRightPanel");
-            // 打开左侧抽屉
-            this.userDrawerSwitch = true;
-        },
-        openCreateGroup() {
-            // 关闭右侧面板
-            this.$bus.$emit("closeRightPanel");
-            // 打开左侧抽屉
-            this.groupDrawerSwitch = true;
-        },
-        beforeCloseEditInfo(done) {
-            this.beforeCloseDrawer(
-                done,
-                0,
-                this.userInfo.avatarPath,
-                this.userInfo.name,
-                this.userInfo.password
-            );
-        },
-        beforeCloseCreateGroup(done) {
-            this.beforeCloseDrawer(
-                done,
-                1,
-                this.groupInfo.avatarPath,
-                this.groupInfo.name
-            );
-        },
-        beforeCloseDrawer(done, type, avatarPath, name, password) {
-            if (this.loading) {
-                return;
-            }
-            this.$confirm("确定要提交表单吗？")
-                .then(
-                    () => {
-                        this.loading = true;
-                        console.log("before submit form");
-
-                        this.$api
-                            .updateUserOrAddGroup(
-                                type,
-                                avatarPath,
-                                name,
-                                password
-                            )
-                            .then((res) => {
-                                if (res.data.status == 200) {
-                                    console.log(res.data);
-                                    if (password) {
-                                        this.user.avatarPath = avatarPath;
-                                        this.user.username = name;
-                                        this.user.password = password;
-                                        window.sessionStorage.setItem(
-                                            "user",
-                                            JSON.stringify(this.user)
-                                        );
-                                    }
-                                } else {
-                                    console.log("request error");
-                                }
-                            });
-
-                        this.timer = setTimeout(() => {
-                            done();
-                            // 动画关闭需要一定的时间
-                            setTimeout(() => {
-                                this.loading = false;
-                            }, 400);
-                        }, 2000);
-                    },
-                    () => {
-                        this.cancelForm();
-                    }
-                )
-                .catch((_) => {
-                    console.log(_);
-                });
-        },
-        cancelForm() {
-            this.loading = false;
-            this.userDrawerSwitch = false;
-            this.groupDrawerSwitch = false;
-            clearTimeout(this.timer);
-        },
-
-        handleAvatarSuccess(res, file) {
-            console.log(res, file);
-            if (this.userDrawerSwitch) {
-                this.userInfo.avatarPath = res.data.fileUrl;
+            if (type === 0) {
+                this.userDrawerSwitch.val = true;
             } else {
-                this.groupInfo.avatarPath = res.data.fileUrl;
+                this.groupDrawerSwitch.val = true;
             }
-        },
-        beforeAvatarUpload() {
-            console.log("before avatar upload");
         },
     },
 };
@@ -336,44 +125,5 @@ export default {
     width: 40px;
     font-size: 140%;
     margin-top: 5px;
-}
-.drawer-content {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 20px;
-    .drawer-body {
-        .avatar-uploader {
-            width: 178px;
-            height: 178px;
-            border: 1px dashed #d9d9d9;
-            border-radius: 6px;
-            cursor: pointer;
-            .drawer-avatar {
-                width: 178px;
-                height: 178px;
-            }
-            .avatar-uploader-icon {
-                font-size: 28px;
-                color: #8c939d;
-                width: 178px;
-                height: 178px;
-                line-height: 178px;
-                text-align: center;
-            }
-        }
-        .avatar-uploader:hover {
-            border-color: #409eff;
-        }
-    }
-    .drawer-footer {
-        text-align: center;
-        width: 90%;
-        margin: 0 auto;
-        .drawer-button {
-            width: 48%;
-        }
-    }
 }
 </style>
---
+++ src/components/common/EditDrawer.vue
@@ -0,0 +1,231 @@
+<template>
+    <el-drawer
+        :before-close="beforeClose"
+        :visible.sync="drawerSwitch.val"
+        direction="ltr"
+        ref="drawer"
+    >
+        <div class="drawer-content">
+            <div class="drawer-body">
+                <el-form :model="formData">
+                    <!-- 头像 -->
+                    <el-form-item label="头像：" label-width="60px">
+                        <el-upload
+                            class="avatar-uploader"
+                            :action="fileUploadUrl"
+                            :show-file-list="false"
+                            :on-success="handleAvatarSuccess"
+                            :before-upload="beforeAvatarUpload"
+                        >
+                            <img
+                                v-if="formData.avatarPath"
+                                :src="formData.avatarPath"
+                                class="drawer-avatar"
+                            />
+                            <i
+                                v-else
+                                class="el-icon-plus avatar-uploader-icon"
+                            ></i>
+                        </el-upload>
+                    </el-form-item>
+
+                    <el-form-item
+                        :label="isUser ? '账号' : '名称' + '：'"
+                        label-width="60px"
+                    >
+                        <el-input
+                            v-model="formData.name"
+                            autocomplete="off"
+                        ></el-input>
+                    </el-form-item>
+
+                    <!-- 密码 -->
+                    <el-form-item
+                        label="密码："
+                        prop="pass"
+                        label-width="60px"
+                        v-if="isUser"
+                    >
+                        <el-input
+                            type="password"
+                            v-model="formData.password"
+                            autocomplete="off"
+                        ></el-input>
+                    </el-form-item>
+                </el-form>
+            </div>
+
+            <div class="drawer-footer">
+                <el-button @click="cancelForm" class="drawer-button"
+                    >取 消</el-button
+                >
+                <el-button
+                    type="primary"
+                    :loading="loading"
+                    @click="$refs.drawer.closeDrawer()"
+                    class="drawer-button"
+                >
+                    {{
+                        loading
+                            ? !isUser
+                                ? "创建中..."
+                                : "提交中..."
+                            : !isUser
+                            ? "创建"
+                            : "确 定"
+                    }}
+                </el-button>
+            </div>
+        </div>
+    </el-drawer>
+</template>
+
+<script>
+import { API_BASE_URL, FILE_UPLOAD_API_URL } from "@/config/api";
+export default {
+    props: {
+        isUser: Boolean,
+        drawerSwitch: Object,
+    },
+    data() {
+        return {
+            fileUploadUrl: API_BASE_URL + FILE_UPLOAD_API_URL,
+            loading: false,
+            timer: null,
+            user: JSON.parse(window.sessionStorage.getItem("user")),
+            formData: {},
+        };
+    },
+    watch: {
+        "drawerSwitch.val": function (newVal, oldVal) {
+            if (newVal === true) {
+                this.formData = this.isUser
+                    ? {
+                          avatarPath: this.user.avatarPath,
+                          name: this.user.username,
+                          password: this.user.password,
+                      }
+                    : {
+                          avatarPath: "",
+                          name: "",
+                      };
+            }
+        },
+    },
+    methods: {
+        beforeClose(done) {
+            this.beforeCloseDrawer(
+                done,
+                this.isUser ? 0 : 1,
+                this.formData.avatarPath,
+                this.formData.name,
+                this.formData.password
+            );
+        },
+        beforeCloseDrawer(done, type, avatarPath, name, password) {
+            if (this.loading) {
+                return;
+            }
+            this.$confirm("确定要提交表单吗？")
+                .then(
+                    () => {
+                        this.loading = true;
+                        console.log("before submit form");
+
+                        this.$api
+                            .updateUserOrAddGroup(
+                                type,
+                                avatarPath,
+                                name,
+                                password
+                            )
+                            .then((res) => {
+                                if (res.data.status == 200) {
+                                    console.log(res.data);
+                                    if (password) {
+                                        this.user.avatarPath = avatarPath;
+                                        this.user.username = name;
+                                        this.user.password = password;
+                                        window.sessionStorage.setItem(
+                                            "user",
+                                            JSON.stringify(this.user)
+                                        );
+                                    }
+                                } else {
+                                    console.log("request error");
+                                }
+                            });
+
+                        this.timer = setTimeout(() => {
+                            done();
+                            // 动画关闭需要一定的时间
+                            setTimeout(() => {
+                                this.loading = false;
+                            }, 400);
+                        }, 2000);
+                    },
+                    () => {
+                        this.cancelForm();
+                    }
+                )
+                .catch((_) => {
+                    console.log(_);
+                });
+        },
+        cancelForm() {
+            this.loading = false;
+            this.drawerSwitch.val = false;
+            clearTimeout(this.timer);
+        },
+        handleAvatarSuccess(res, file) {
+            console.log(res, file);
+            this.formData.avatarPath = res.data.fileUrl;
+        },
+        beforeAvatarUpload() {
+            console.log("before avatar upload");
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.drawer-content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 20px;
+    .drawer-body {
+        .avatar-uploader {
+            width: 178px;
+            height: 178px;
+            border: 1px dashed #d9d9d9;
+            border-radius: 6px;
+            cursor: pointer;
+            .drawer-avatar {
+                width: 178px;
+                height: 178px;
+            }
+            .avatar-uploader-icon {
+                font-size: 28px;
+                color: #8c939d;
+                width: 178px;
+                height: 178px;
+                line-height: 178px;
+                text-align: center;
+            }
+        }
+        .avatar-uploader:hover {
+            border-color: #409eff;
+        }
+    }
+    .drawer-footer {
+        text-align: center;
+        width: 90%;
+        margin: 0 auto;
+        .drawer-button {
+            width: 48%;
+        }
+    }
+}
+</style>
